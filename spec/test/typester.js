c393544014c4f4f5e2b25ac4861550a6beb79fce
Dominic Chambers <dominic.chambers@gmail.com>
2014-10-24 07:56:56 +0100

    Typester now verifies the arguments it itself receives so it can fail-fast if improperly used.

--- spec/test/typester.js
+++ spec/test/typester.js
@@ -3,65 +3,98 @@ var using = typester.using;
 var ArgumentError = typester.ArgumentError;
 
 describe('typester', function() {
-	it('throws an ArgumentError if we try to validate a non-existent argument', function() {
-    function func() {
-      using(arguments)
-        .verify('num').isA(Number);
-    }
+	describe('incorrect usage', function() {
+		it('throws a ArgumentError if using() is invoked without arguments', function() {
+			function func() {
+				using();
+			}
 
-    func.should.throw('num argument must be provided');
-    func.should.throw(ArgumentError);
-		func.bind(func, 10).should.not.throw();
+			func.should.throw('arguments argument must be provided');
+			func.should.throw(ArgumentError);
+		});
+
+		it('throws a ArgumentError if verify() is invoked without arguments', function() {
+			function func() {
+				using([10])
+					.verify().isA(Number);
+			}
+
+			func.should.throw('argName argument must be provided');
+			func.should.throw(ArgumentError);
+		});
+
+		it('throws a TypeError if verify() is passed something other than an Array', function() {
+			function func(num) {
+				using(arguments)
+					.verify(num).isA(Number);
+			}
+
+			func.bind(func, 10).should.throw('argName argument must be a String');
+			func.bind(func, 10).should.throw(TypeError);
+		});
 	});
 
-	it('throws an ArgumentError if we try to validate a subsequent non-existent argument', function() {
-		function func() {
-			using(arguments)
-				.verify('bool').isA(Boolean)
-				.verify('num').isA(Number);
-		}
+	describe('validation', function() {
+		it('throws an ArgumentError if we try to validate a non-existent argument', function() {
+			function func() {
+				using(arguments)
+					.verify('num').isA(Number);
+			}
 
-		func.bind(func, true).should.throw('num argument must be provided');
-		func.bind(func, true).should.throw(ArgumentError);
-		func.bind(func, true, 10).should.not.throw();
-	});
+			func.should.throw('num argument must be provided');
+			func.should.throw(ArgumentError);
+			func.bind(func, 10).should.not.throw();
+		});
 
-	it('throws a TypeError if we try to validate a provided argument is of the wrong type', function() {
-		function func() {
-			using([true])
-				.verify('num').isA(Number);
-		}
+		it('throws an ArgumentError if we try to validate a subsequent non-existent argument', function() {
+			function func() {
+				using(arguments)
+					.verify('bool').isA(Boolean)
+					.verify('num').isA(Number);
+			}
 
-		func.should.throw(TypeError);
-		func.should.throw('num argument must be a Number');
-	});
+			func.bind(func, true).should.throw('num argument must be provided');
+			func.bind(func, true).should.throw(ArgumentError);
+			func.bind(func, true, 10).should.not.throw();
+		});
 
-	it('does not require optional arguments to be provided, but throws if they are incorrect', function() {
-		function func() {
-			using(arguments)
-				.verify('bool').isA(Boolean)
-				.verify('num').optionally.isA(Number);
-		}
+		it('throws a TypeError if we try to validate a provided argument is of the wrong type', function() {
+			function func() {
+				using([true])
+					.verify('num').isA(Number);
+			}
 
-		func.bind(func, true).should.not.throw();
-		func.bind(func, true, 10).should.not.throw();
-		func.bind(func, true, '10').should.throw(TypeError);
-		func.bind(func, true, '').should.throw(TypeError);
-		func.bind(func, true, []).should.throw(TypeError);
-	});
+			func.should.throw(TypeError);
+			func.should.throw('num argument must be a Number');
+		});
 
-	it('allows mandatory arguments to appear after an optional argument', function() {
-		function func() {
-			using(arguments)
-				.verify('num').optionally.isA(Number)
-				.verify('bool').isA(Boolean);
-		}
+		it('does not require optional arguments to be provided, but throws if they are incorrect', function() {
+			function func() {
+				using(arguments)
+					.verify('bool').isA(Boolean)
+					.verify('num').optionally.isA(Number);
+			}
 
-		func.bind(func, null, true).should.not.throw();
-		func.bind(func, undefined, true).should.not.throw();
-		func.bind(func, null).should.throw(ArgumentError);
-		func.bind(func, null, {}).should.throw(TypeError);
-		func.bind(func, '10', true).should.throw(TypeError);
-		func.bind(func, '10', true).should.throw('num argument must be a Number');
+			func.bind(func, true).should.not.throw();
+			func.bind(func, true, 10).should.not.throw();
+			func.bind(func, true, '10').should.throw(TypeError);
+			func.bind(func, true, '').should.throw(TypeError);
+			func.bind(func, true, []).should.throw(TypeError);
+		});
+
+		it('allows mandatory arguments to appear after an optional argument', function() {
+			function func() {
+				using(arguments)
+					.verify('num').optionally.isA(Number)
+					.verify('bool').isA(Boolean);
+			}
+
+			func.bind(func, null, true).should.not.throw();
+			func.bind(func, undefined, true).should.not.throw();
+			func.bind(func, null).should.throw(ArgumentError);
+			func.bind(func, null, {}).should.throw(TypeError);
+			func.bind(func, '10', true).should.throw(TypeError);
+			func.bind(func, '10', true).should.throw('num argument must be a Number');
+		});
 	});
 });
