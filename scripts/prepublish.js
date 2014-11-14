e3b5de9e14224864574c6ddc725616c2d33b3fe7
Dominic Chambers <dominic.chambers@gmail.com>
2014-11-14 23:08:36 +0000

    Bundled the performance test so we can run it within different browsers.

--- scripts/prepublish.js
+++ scripts/prepublish.js
@@ -4,5 +4,6 @@ var build = require('dchambers-lib-build-tool');
 
 build.bundleDeps(
 	build.bundle('typester.js', ['./lib/global-typester.js']),
-	build.bundle('typester-spec-tests.js', ['./spec/test/tests.spec.js'])
+	build.bundle('typester-spec-tests.js', ['./spec/test/tests.spec.js']),
+	build.bundle('typester-performance-test.js', ['./scripts/test-performance.js'])
 );
